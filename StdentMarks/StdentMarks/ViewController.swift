c24cc7161e87594528a7d1a3a28919095d5201da
Jyothi Dronamraju <jyothi.dronam@gmail.com>
2024-01-25 10:50:29 -0600

    Add files via upload

---
+++ StdentMarks/StdentMarks/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  StdentMarks
+//
+//  Created by Dronamraju,Naga Jyothi on 1/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var inputOL: UITextField!
+    
+    
+
+    @IBOutlet weak var marksInputOL: UITextField!
+    
+    
+    @IBOutlet weak var outputOL: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func CheckBtnClicked(_ sender: Any) {
+    }
+    
+}
+
